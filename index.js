dbd994f89dcc3f474841ccfad56c91ad872237b8
Austin Chen <akrolsmir@gmail.com>
2012-10-02 20:48:41 -0700

    adding index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(".stuffs").css("color", "red");
+	$("h1").css("position", "absolute");
+	setTimeout(function(){
+		$("h1").animate({
+				left: "400px",
+				opacity: 0
+			}, 1000);
+		}, 3000);
+});
